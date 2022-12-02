ebdacaf25d779817928a3f325ed2f3f4c3e371aa
ikeepo <jizhijixing@163.com>
2022-12-02 12:01:03 +0800

    pass

---
+++ os3/src/task/task.rs
@@ -0,0 +1,17 @@
+use super::TaskContext;
+#[derive(Copy, Clone)]
+/// task control block structure
+pub struct TaskControlBlock {
+    pub task_status: TaskStatus,
+    pub task_cx: TaskContext,
+    // LAB1: Add whatever you need about the Task.
+}
+
+#[derive(Copy, Clone, PartialEq)]
+/// task status: UnInit, Ready, Running, Exited
+pub enum TaskStatus {
+    UnInit,
+    Ready,
+    Running,
+    Exited,
+}
